89b971496a5c6cdc166ea5735f9e50907a1e4c3e
sagarbhmr@gmail.com <sagarbhr@gmail.com>
2025-01-31 18:26:29 +0530

    feed backend api code initiation

--- routes/feed.js
+++ routes/feed.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { body } = require('express-validator');
 
 const feedController = require('../controllers/feed')
 
@@ -7,6 +8,9 @@ const router = express.Router();
 // GET /feed/posts
 router.get('/posts', feedController.getPosts);
 
-router.post('/post', feedController.createPost);
+router.post('/post',[
+    body('title').trim().isLength({min:5}),
+    body('content').trim().isLength({min:5})
+], feedController.createPost);
 
 module.exports = router;
