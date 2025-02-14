92ec2ba1195067bb6143dd157461316a2cd91c74
sagarbhmr@gmail.com <sagarbhr@gmail.com>
2025-02-15 04:56:24 +0530

    delete post code added

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const path = require("path");
-const fs = require("fs");
+
 const express = require("express");
 const mongoose = require("mongoose");
 const multer = require('multer');
@@ -12,6 +12,7 @@ const cors = require( `cors` );
 const bodyParser = require("body-parser");
 const resolvers = require("./graphql/resolvers");
 const auth = require('./middleware/auth');
+const { clearImage } =  require('./util/file');
 const app = express();
 
 const fileStorage = multer.diskStorage({
@@ -102,10 +103,4 @@ mongoose
     app.listen(8080);
   })
   .catch(err => console.log(err));
-
-
-  const clearImage = filePath => {
-    filePath = path.join(__dirname, '..', filePath);
-    fs.unlink(filePath, err => console.log(err));
-  };
   
